6135cdee4d519baaae150e2446907f9ef1ca5b05
DanendraPradnya <danendrapradnya@gmail.com>
2024-12-10 23:45:17 +0800

    Aku mau ngepull cok!

--- resources/views/musang.blade.php
+++ resources/views/musang.blade.php
@@ -6,6 +6,7 @@
     <title>Landing Page - MUSANG HMTI 2024</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link rel="icon" href="{{ asset('gambar/LOGO MUSANG 2024 rz.png') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/about.css') }}">
     <link rel="stylesheet" href="{{ asset('css/rekam.css') }}">
